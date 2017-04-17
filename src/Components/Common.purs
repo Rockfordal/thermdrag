62011b273306282bbd6e385b339b81c028627d0c
Anders Lundström <lundstrom123@gmail.com>
2017-04-18 00:33:05 +0200

    line refactor

--- src/Components/Common.purs
+++ src/Components/Common.purs
@@ -11,13 +11,15 @@ rawview h = T.simpleSpec T.defaultPerformAction render
   where
   render _ _ _ _ = h
 
+
 linje :: Int -> Int -> Int -> Int -> ReactElement 
 linje x1 y1 x2 y2 =
   S.line
-    [ unsafeMkProps "x1" x1
-    , unsafeMkProps "y1" y1
-    , unsafeMkProps "x2" x2
-    , unsafeMkProps "y2" y2
-    , unsafeMkProps "stroke" "black"
-    , unsafeMkProps "stroke-width" 2 
+    [ p "x1" x1
+    , p "y1" y1
+    , p "x2" x2
+    , p "y2" y2
+    , p "stroke" "black"
+    , p "stroke-width" 2 
     ] []
+    where p = unsafeMkProps
